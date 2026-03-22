67359c2838cc05a377446fc326f4b604b975c6e9
AzureMind <46114014+patstew123@users.noreply.github.com>
2026-03-22 21:29:48 +0000

    New code cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        posts: resolve(__dirname, 'posts.html'),
-      },
-    },
-  },
-})
+export default defineConfig({})
